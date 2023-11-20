b70b6782634c01e03d0bc2ab4b5391abe4f58350
Vanshaj Gupta <vanshajgupta5101@gmail.com>
2023-11-19 18:35:41 -0700

    scatter plot

--- vanshaj.js
+++ vanshaj.js
@@ -10,9 +10,15 @@ var height
 var chartHeight
 var chartWidth
 var pieData = []
+var hierData = []
+var outerPieData = []
 var colors
 var colorCodes = ["#F22B67", "#F69402", "#F1DF01", "#50E600", "#02A4ED"]
-// "#7E25D7", "#00798D"
+var toolTipData = []
+var ageGrps = ["0-10", "11-20", "21-30", "31-40", "41-50", "51-60", "61-70", "71-80"]
+var colorScale = d3.scaleOrdinal()
+    .domain(ageGrps)
+    .range(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f"]);
 
 document.addEventListener("DOMContentLoaded", function () {
     var svg = d3.select("#vanshaj");
@@ -42,11 +48,8 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function plotInnovativeChart(data) {
-    console.log("------->", data)
     globalExpenseDataset.forEach(d => {
-
         if (!educationLevelList.includes(d.EducationLevel)) {
-
             educationLevelList.push(d.EducationLevel)
         }
         if (!ageGroupList.includes(d.AgeGroup)) {
@@ -56,24 +59,7 @@ function plotInnovativeChart(data) {
             categoryList.push(d.category)
         }
     })
-    var svg = d3.select("#vanshaj")
-    height = +svg.style("height").replace("px", '');
-    width = +svg.style("width").replace("px", '');
 
-    chartHeight = height - margin.top - margin.bottom
-    chartWidth = width - margin.left - margin.right
-
-    const g = svg.append("g")
-        .attr('transform', `translate(${margin.left}, ${margin.top})`)
-
-    var xScale = d3.scaleLinear()
-        .domain([0, 15])
-        .range([0, chartWidth])
-
-    var xAxis = d3.axisBottom(xScale)
-
-    g.append('g').call(xAxis)
-        .attr('transform', `translate(0,${chartHeight})`)
 
 
     for (var i = 1; i <= 15; i++) {
@@ -114,82 +100,249 @@ function plotInnovativeChart(data) {
         })
         // console.log(i, totolWage, totalShelter, totalEducation, totalRentAdjustment, totalFood, totalRecreation)
 
+
+
         pieData[i - 1] = {
             "MonthNumber": i,
+            "TotalWage": totolWage,
             "Saving": parseFloat((totolWage - totalShelter - totalEducation + totalRentAdjustment - totalFood - totalRecreation).toFixed(2)),
-            "Shelter": -1 * parseFloat(totalShelter.toFixed(2)),
-            "Education": -1 * parseFloat(totalEducation.toFixed(2)),
-            "Food": -1 * parseFloat(totalFood.toFixed(2)),
-            "Recreation": -1 * parseFloat(totalRecreation.toFixed(2)),
+            "ExpenseData": {
+                "Shelter": -1 * parseFloat(totalShelter.toFixed(2)),
+                "Education": -1 * parseFloat(totalEducation.toFixed(2)),
+                "Food": -1 * parseFloat(totalFood.toFixed(2)),
+                "Recreation": -1 * parseFloat(totalRecreation.toFixed(2))
+            },
+            "TotalExpense": -1 * (parseFloat(totalShelter.toFixed(2)) + parseFloat(totalEducation.toFixed(2)) + parseFloat(totalFood.toFixed(2)) + parseFloat(totalRecreation.toFixed(2))),
             "LengthOfData": lengthOfData,
             "AvgExpense": (-1 * parseFloat((totalEducation + totalFood + totalRecreation + totalShelter).toFixed(2))) / lengthOfData
         }
+
+
     }
 
-    console.log(pieData)
-    var arc = d3.arc()
-        .innerRadius(0)
-        .outerRadius(function (d) {
-            return 50
-        })
 
-    console.log(Object.entries(pieData))
-    var pie = d3.pie()
-        .sort(null)
-        .value(function (d) {
-            var lst = []
-            lst.push(d[1].Saving)
-            lst.push(d[1].Shelter)
-            lst.push(d[1].Education)
-            lst.push(d[1].Food)
-            lst.push(d[1].Recreation)
-            console.log("pie---------->>>>>>>", d[1], lst)
-            return lst
-        })
-    colors = d3.scaleOrdinal().domain(["Saving", "Shelter", "Education", "Food", "Recreation"]).range(colorCodes)
-
-    var pieCharts = svg.selectAll('g')
-        .data(pieData)
-        .enter()
-        .append("g")
-        .property("radius", function (d) { return d.AvgExpense })
-        .attr("transform", function (d) { return `translate(${d.MonthNumber}, ${chartHeight / 2})` })
-
-    var finalPieData = pie(Object.entries(pieData))
-    pieCharts.selectAll()
-    .data(finalPieData)
-    .enter()
-    .append("path")
-    .attr("d", arc)
-    .attr("fill", function(d, i){
-        console.log(i)
-        var x = d3.schemeCategory10
-        return x[i]
-    })
+    plotPieScatter(pieData)
 
 
-    // var simulation = d3.forceSimulation(pieData)
-    //     .force('charge', d3.forceManyBody().strength(4))
-    //     .force('x', d3.forceX().x(function (d) {
-    //         return xScale(d.MonthNumber)
-    //     }))
-    //     .force('y', d3.forceY().y(function (d) {
-    //         return chartHeight / 2
-    //     }))
-    //     .force('collision', d3.forceCollide().radius(function (d) {
-    //         return d.AvgExpense
-    //     }))
 
-    // var numIterations = 10
-    // for (let i = 0; i < numIterations; i++) {
-    //     simulation.tick();
-    // }
 
-    // simulation.on('end', (d) => {
-    //     var svg = d3.select("#vanshaj")
-    //     svg.selectAll("circle")
-    // })
+
+
 
 
 }
 
+
+function plotPieScatter(data) {
+    var svg = d3.select("#vanshaj")
+    height = +svg.style("height").replace("px", '');
+    width = +svg.style("width").replace("px", '');
+    var margin = { top: 10, bottom: 30, left: 35, right: 20 }
+    chartHeight = height - margin.top - margin.bottom
+    chartWidth = width - margin.left - margin.right - 400
+
+    const g = svg.append("g")
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+    var xScale = d3.scaleLinear()
+        .domain([0, 15])
+        .range([0, chartWidth])
+
+    var xAxis = d3.axisBottom(xScale)
+
+
+    g.append('g').call(xAxis)
+        .attr('transform', `translate(0,${chartHeight})`)
+
+    var toolTipDiv = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+
+    var tooltip = document.querySelector('.tooltip');
+
+    var tooltipStyle = window.getComputedStyle(tooltip);
+
+    var tooltipWidth = tooltipStyle.width;
+    var tooltipHeight = tooltipStyle.height;
+
+    tooltipWidth = parseFloat(tooltipWidth);
+    tooltipHeight = parseFloat(tooltipHeight);
+    var toolTipMargin = { top: 30, bottom: 30, left: 30, right: 30 }
+
+    var innerBarWidth = tooltipWidth - toolTipMargin.left - toolTipMargin.right
+    var innerBarHeight = tooltipHeight - toolTipMargin.top - toolTipMargin.bottom
+
+    var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(function (d) {
+            return d.Saving
+        })
+
+    var pie = d3.pie()
+        .sort(null)
+        .value(function (d) {
+            return d[1]
+        })
+
+
+    // colors = d3.scaleOrdinal().domain(["Saving", "Shelter", "Education", "Food", "Recreation"]).range(colorCodes)
+    colors = d3.scaleOrdinal().domain(["Shelter", "Education", "Food", "Recreation"]).range(colorCodes)
+
+
+
+    var pieRadiusFunct = d3.scaleSqrt().domain(d3.extent(data.map((d) => d.Saving))).range([30, 60])
+    var pies = g.selectAll(".pieGroup")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "pieGroup")
+        .property("radius", function (d) {
+            return d.Saving
+        })
+        .property("index", function (d) {
+            return d.MonthNumber
+        })
+        .attr("transform", function (d) {
+            return `translate(${xScale(d.MonthNumber)}, ${chartHeight / 2})`
+        })
+
+    var toolTipSvg = toolTipDiv.append("svg")
+        .attr("id", "toolTipSvg")
+        .attr("width", tooltipWidth)
+        .attr("height", tooltipHeight)
+    pies.selectAll(".pieScatter")
+        .data(function (d) {
+            return pie(Object.entries(d.ExpenseData))
+        })
+        .enter()
+        .append("path")
+        .attr("class", "pieScatter")
+        .attr("d", function (d) {
+            var radius = d3.select(this.parentNode).property("radius")
+            arc.outerRadius(pieRadiusFunct(radius))
+            return arc(d)
+        })
+        .attr("fill", function (d) {
+            return colors(d.data[0])
+        })
+
+        .attr("stroke", function (d) {
+            return colors(d.data[0])
+        })
+        .attr("stroke-width", 2)
+        .on("mouseover", function (event, data) {
+            var ind = d3.select(this.parentNode).property("index")
+
+            var ageExpenseDict = {}
+            console.log("before----", ageExpenseDict)
+            globalExpenseDataset.forEach(function (d) {
+                if (d.MonthNumber <= ind) {
+                    if (ind == d.MonthNumber && d.Category == data.data[0]) {
+                        if (d.AgeGroup in ageExpenseDict) {
+                            ageExpenseDict[d.AgeGroup] += -1 * (parseInt(d.Amount))
+                        } else {
+                            ageExpenseDict[d.AgeGroup] = -1 * (parseInt(d.Amount))
+                        }
+                    }
+                }
+            })
+
+            var yScale = d3.scaleLinear()
+                .domain([0, +d3.max(Object.values(ageExpenseDict))])
+                .range([innerBarHeight, 0])
+            var xAxisLabels = Object.keys(ageExpenseDict).sort((a, b) => {
+                const [aMin, aMax] = a.split('-').map(Number);
+                const [bMin, bMax] = b.split('-').map(Number);
+                return aMin - bMin || aMax - bMax;
+            })
+            var xScale = d3.scaleBand()
+                .domain(xAxisLabels)
+                .range([0, innerBarWidth])
+                .padding(0.1)
+
+            var xAxis = d3.axisBottom(xScale)
+            var yAxis = d3.axisLeft(yScale)
+
+
+
+
+            var toolTipG = toolTipSvg.append('g')
+                .attr('transform', `translate(${20}, ${5})`)
+
+            toolTipG.append('g').call(xAxis)
+                .attr('transform', `translate(0,${innerBarHeight})`)
+                .selectAll('text')
+                .style("text-anchor", "end")
+
+            toolTipG.append('g').call(yAxis)
+
+
+            toolTipG.selectAll("rect")
+                .data(Object.entries(ageExpenseDict))
+                .enter()
+                .append("rect")
+                .attr("x", function (d) {
+
+                    return xScale(d[0])
+                })
+                .attr("y", function (d) {
+                    return yScale(d[1])
+                })
+                .attr('width', xScale.bandwidth() - 5)
+                .attr('height', d => innerBarHeight - yScale(d[1]))
+                .attr("fill", function (d) {
+                    return colorScale(d[0])
+                })
+            toolTipDiv.transition()
+                .duration(0)
+                .style("display", "block")
+                .style("opacity", 0.8)
+                .style("left", event.pageX + "px")
+                .style("top", event.pageY + 'px')
+        })
+        .on("mousemove", function (d, i) {
+            toolTipDiv.transition()
+                .duration(0)
+                .style("display", "block")
+                .style("opacity", 0.8)
+                .style("left", d.pageX + "px")
+                .style("top", d.pageY + 'px')
+        })
+        .on('mouseout', function (d) {
+            var tsvg = d3.select("#toolTipSvg")
+            tsvg.selectAll("*").remove()
+
+            toolTipDiv.transition()
+                .duration(300)
+                .style("display", "none")
+            toolTipDiv.innerHTML = ""
+        })
+
+
+    var simulation = d3.forceSimulation(data)
+        .force('charge', d3.forceManyBody().strength(4))
+        .force('x', d3.forceX().x(function (d) {
+            return xScale(d.MonthNumber)
+        }))
+        .force('y', d3.forceY().y(function (d) {
+            return chartHeight / 2
+        }))
+        .force('collision', d3.forceCollide().radius(function (d) {
+            return pieRadiusFunct(d.Saving)
+        }))
+
+    var numIterations = 10
+    for (let i = 0; i < numIterations; i++) {
+        simulation.tick()
+    }
+
+    simulation.on("end", (d) => {
+        g.selectAll(".pieGroup")
+            .attr("transform", function (d) {
+                return `translate(${d.x}, ${d.y})`
+            })
+    })
+
+
+
+}
