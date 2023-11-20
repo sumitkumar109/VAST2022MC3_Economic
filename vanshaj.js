4903b5995d1f4b7b940daf34043dbe4fce679c5d
Vanshaj Gupta <vanshajgupta5101@gmail.com>
2023-11-20 13:04:03 -0700

    data

--- vanshaj.js
+++ vanshaj.js
@@ -190,133 +190,8 @@ function plotPieScatter(data) {
 
 
 
+
     var pieRadiusFunct = d3.scaleSqrt().domain(d3.extent(data.map((d) => d.Saving))).range([30, 60])
-    var pies = g.selectAll(".pieGroup")
-        .data(data)
-        .enter()
-        .append("g")
-        .attr("class", "pieGroup")
-        .property("radius", function (d) {
-            return d.Saving
-        })
-        .property("index", function (d) {
-            return d.MonthNumber
-        })
-        .attr("transform", function (d) {
-            return `translate(${xScale(d.MonthNumber)}, ${chartHeight / 2})`
-        })
-
-    var toolTipSvg = toolTipDiv.append("svg")
-        .attr("id", "toolTipSvg")
-        .attr("width", tooltipWidth)
-        .attr("height", tooltipHeight)
-    pies.selectAll(".pieScatter")
-        .data(function (d) {
-            return pie(Object.entries(d.ExpenseData))
-        })
-        .enter()
-        .append("path")
-        .attr("class", "pieScatter")
-        .attr("d", function (d) {
-            var radius = d3.select(this.parentNode).property("radius")
-            arc.outerRadius(pieRadiusFunct(radius))
-            return arc(d)
-        })
-        .attr("fill", function (d) {
-            return colors(d.data[0])
-        })
-
-        .attr("stroke", function (d) {
-            return colors(d.data[0])
-        })
-        .attr("stroke-width", 2)
-        .on("mouseover", function (event, data) {
-            var ind = d3.select(this.parentNode).property("index")
-
-            var ageExpenseDict = {}
-            console.log("before----", ageExpenseDict)
-            globalExpenseDataset.forEach(function (d) {
-                if (d.MonthNumber <= ind) {
-                    if (ind == d.MonthNumber && d.Category == data.data[0]) {
-                        if (d.AgeGroup in ageExpenseDict) {
-                            ageExpenseDict[d.AgeGroup] += -1 * (parseInt(d.Amount))
-                        } else {
-                            ageExpenseDict[d.AgeGroup] = -1 * (parseInt(d.Amount))
-                        }
-                    }
-                }
-            })
-
-            var yScale = d3.scaleLinear()
-                .domain([0, +d3.max(Object.values(ageExpenseDict))])
-                .range([innerBarHeight, 0])
-            var xAxisLabels = Object.keys(ageExpenseDict).sort((a, b) => {
-                const [aMin, aMax] = a.split('-').map(Number);
-                const [bMin, bMax] = b.split('-').map(Number);
-                return aMin - bMin || aMax - bMax;
-            })
-            var xScale = d3.scaleBand()
-                .domain(xAxisLabels)
-                .range([0, innerBarWidth])
-                .padding(0.1)
-
-            var xAxis = d3.axisBottom(xScale)
-            var yAxis = d3.axisLeft(yScale)
-
-
-
-
-            var toolTipG = toolTipSvg.append('g')
-                .attr('transform', `translate(${20}, ${5})`)
-
-            toolTipG.append('g').call(xAxis)
-                .attr('transform', `translate(0,${innerBarHeight})`)
-                .selectAll('text')
-                .style("text-anchor", "end")
-
-            toolTipG.append('g').call(yAxis)
-
-
-            toolTipG.selectAll("rect")
-                .data(Object.entries(ageExpenseDict))
-                .enter()
-                .append("rect")
-                .attr("x", function (d) {
-
-                    return xScale(d[0])
-                })
-                .attr("y", function (d) {
-                    return yScale(d[1])
-                })
-                .attr('width', xScale.bandwidth() - 5)
-                .attr('height', d => innerBarHeight - yScale(d[1]))
-                .attr("fill", function (d) {
-                    return colorScale(d[0])
-                })
-            toolTipDiv.transition()
-                .duration(0)
-                .style("display", "block")
-                .style("opacity", 0.8)
-                .style("left", event.pageX + "px")
-                .style("top", event.pageY + 'px')
-        })
-        .on("mousemove", function (d, i) {
-            toolTipDiv.transition()
-                .duration(0)
-                .style("display", "block")
-                .style("opacity", 0.8)
-                .style("left", d.pageX + "px")
-                .style("top", d.pageY + 'px')
-        })
-        .on('mouseout', function (d) {
-            var tsvg = d3.select("#toolTipSvg")
-            tsvg.selectAll("*").remove()
-
-            toolTipDiv.transition()
-                .duration(300)
-                .style("display", "none")
-            toolTipDiv.innerHTML = ""
-        })
 
 
     var simulation = d3.forceSimulation(data)
@@ -336,10 +211,158 @@ function plotPieScatter(data) {
         simulation.tick()
     }
 
-    simulation.on("end", (d) => {
-        g.selectAll(".pieGroup")
+    simulation.on("end", (finalEvent) => {
+
+        var pies = g.selectAll(".pieGroup")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("class", "pieGroup")
+            .property("radius", function (d) {
+                return d.Saving
+            })
+            .property("index", function (d) {
+                return d.MonthNumber
+            })
             .attr("transform", function (d) {
                 return `translate(${d.x}, ${d.y})`
+            })
+
+        var toolTipSvg = toolTipDiv.append("svg")
+            .attr("id", "toolTipSvg")
+            .attr("width", tooltipWidth)
+            .attr("height", tooltipHeight)
+        pies.selectAll(".pieScatter")
+            .data(function (d) {
+                return pie(Object.entries(d.ExpenseData))
+            })
+            .enter()
+            .append("path")
+            .attr("class", "pieScatter")
+            .attr("d", function (d) {
+                var radius = d3.select(this.parentNode).property("radius")
+                arc.outerRadius(pieRadiusFunct(radius))
+                return arc(d)
+            })
+            .attr("fill", function (d) {
+                return colors(d.data[0])
+            })
+
+            .attr("stroke", function (d) {
+                return colors(d.data[0])
+            })
+            .attr("stroke-width", 2)
+            .on("mouseover", function (event, data) {
+                var ind = d3.select(this.parentNode).property("index")
+
+                var ageExpenseDict = {}
+                globalExpenseDataset.forEach(function (d) {
+                    if (d.MonthNumber <= ind) {
+                        if (ind == d.MonthNumber && d.Category == data.data[0]) {
+                            if (d.AgeGroup in ageExpenseDict) {
+                                ageExpenseDict[d.AgeGroup] += -1 * (parseInt(d.Amount))
+                            } else {
+                                ageExpenseDict[d.AgeGroup] = -1 * (parseInt(d.Amount))
+                            }
+                        }
+                    }
+                })
+                var yScale = d3.scaleLinear()
+                    .domain([0, d3.max(Object.values(ageExpenseDict))])
+                    .range([innerBarHeight, 0])
+                var xAxisLabels = Object.keys(ageExpenseDict).sort((a, b) => {
+                    const [aMin, aMax] = a.split('-').map(Number);
+                    const [bMin, bMax] = b.split('-').map(Number);
+                    return aMin - bMin || aMax - bMax;
+                })
+                var xScale = d3.scaleBand()
+                    .domain(xAxisLabels)
+                    .range([0, innerBarWidth])
+                    .padding(0.1)
+
+                var xAxis = d3.axisBottom(xScale)
+                var yAxis = d3.axisLeft(yScale)
+
+
+
+                var txt = "Age group vs " + data.data[0] + " Expense"
+                var toolTipG = toolTipSvg.append('g')
+                    .attr('transform', `translate(${50}, ${20})`)
+                toolTipSvg.append("text")
+                    .attr("x", innerBarWidth / 2 + 20)  // Adjust the x-coordinate as needed
+                    .attr("y", 10)  // Adjust the y-coordinate as needed
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "16px")
+                    .style("font-weight", "bold")
+                    .text(txt)
+
+                toolTipG.append('g').call(xAxis)
+                    .attr('transform', `translate(0,${innerBarHeight})`)
+                    .selectAll('text')
+                    .style("text-anchor", "end")
+
+                toolTipG.append('g').call(yAxis)
+
+
+                toolTipG.selectAll("rect")
+                    .data(Object.entries(ageExpenseDict))
+                    .enter()
+                    .append("rect")
+                    .attr("x", function (d) {
+
+                        return xScale(d[0])
+                    })
+                    .attr("y", function (d) {
+                        return yScale(d[1])
+                    })
+                    .attr('width', xScale.bandwidth() - 10)
+                    .attr('height', d => innerBarHeight - yScale(d[1]))
+                    .attr("fill", function (d) {
+                        return colorScale(d[0])
+                    })
+
+                toolTipG.selectAll(".barText")
+                    .data(Object.entries(ageExpenseDict))
+                    .enter()
+                    .append("text")
+                    .attr("class", "barText")
+                    .text(function (d) {
+                        colorScale.log(d[1])
+                        return d[1]; // Use the data value as the text content
+                    })
+                    .attr("x", function (d) {
+                        return xScale(d[0]) + xScale.bandwidth() / 2;
+                    })
+                    .attr("y", function (d) {
+                        return yScale(d[1]) - 5; // Adjust the y-coordinate for text positioning
+                    })
+                    .attr("text-anchor", "middle")
+                    .style("font-size", "12px")
+                    .style("fill", "white")
+                toolTipDiv.transition()
+                    .duration(0)
+                    .style("display", "block")
+                    .style("opacity", 0.8)
+                    .style("left", event.pageX + "px")
+                    .style("top", event.pageY + 'px')
+
+            })
+            .on("mousemove", function (d, i) {
+
+                toolTipDiv.transition()
+                    .duration(0)
+                    .style("display", "block")
+                    .style("opacity", 0.8)
+                    .style("left", d.pageX + "px")
+                    .style("top", d.pageY + 'px')
+            })
+            .on('mouseout', function (d) {
+                var tsvg = d3.select("#toolTipSvg")
+                tsvg.selectAll("*").remove()
+
+                toolTipDiv.transition()
+                    .duration(300)
+                    .style("display", "none")
             })
     })
 
